f0bd18c3e595376634de6ee363ac7a0f1dcb9fd2
Jayant Sani <jayant.sani@gmail.com>
2014-03-09 18:25:30 -0400

    Complete most of assignment 2

--- assignment2/question1.py
+++ assignment2/question1.py
@@ -1,3 +1,6 @@
+import datetime
+import re
+
 class Content(object):
     '''
     Required properties:
@@ -9,7 +12,42 @@ class Content(object):
         - show
         - matches_url (question 1d)
     '''
-    pass
+    def __init__(self, title, subtitle, creator):
+        self.title = title
+        self.subtitle = subtitle
+        self.creator = creator
+        self.publication_date = datetime.date.today()
+
+    def show(self):
+        print "<Title: {0}\nSubtitle: {1}\nCreator: {2}\nPublication Date: {3}>"
+        .format(self.title, self.subtitle, self.creator, self.publication_date.__str__())
+
+    def matches_url(self, url):
+        pattern = r'http://thecrimson.com/(\w+)/(\d{4})/(\d{1,2})/(\d{1,2})/(\w+)'
+        return !(re.search(pattern, url) is None)
+
+    '''
+    Question 1e
+    '''
+    @classmethod
+    def from_url(c_lst, url):
+        matched_content = []
+
+        for i in range(len(c_lst)):
+
+            split_title = c_lst[i].title.split()
+            slug = '-'.join(split_title)
+
+            pattern = r'http://thecrimson.com/(\w+)/{0}/{1}/{2}/{3}/'
+            .format(c_list[i].publication_date.year, c_list[i].publication_date.month, c_list[i].publication_date.day, slug)
+            if (!(re.search(pattern, url) is None): matched_content.append(c_lst[i])
+        
+        if (len(matched_content) > 1): 
+            print "Error - More than one content object matches the URL!"
+        else if:
+            print "No content matched"    
+        else:
+            return matched_content[0]
 
 class Article(Content):
     '''
@@ -19,7 +57,13 @@ class Article(Content):
     Required methods:
         - All methods of Content
     '''
-    pass
+    def __init__(self, title, subtitle, creator, image):
+        super(Content, self).__init__(title, subtitle, creator)
+        self.related_image = image
+
+    def show(self):
+        print "<Title: {0}\n Subtitle: {1}\nCreator: {2}\nPublication Date: {3}\nRelated Image: {4}>"
+        .format(self.title, self.subtitle, self.creator, self.publication_date.__str__(), self.related_image)
 
 class Picture(Content):
     '''
@@ -29,16 +73,19 @@ class Picture(Content):
     Required methods:
         - All methods of Content
     '''
-    pass
+    def __init__(self, title, subtitle, creator, image_file):
+        super(Content, self).__init__(title, subtitle, creator)
+        self.image_file = image_file
 
-'''
-Question 1e
-'''
-def from_url(c_lst, url):
-    pass
+    def show(self):
+        print "<Title: {0}\n Subtitle: {1}\nCreator: {2}\nPublication Date: {3}\nImage File: {4}>"
+        .format(self.title, self.subtitle, self.creator, self.publication_date.__str__(), self.image_file)
 
 '''
 Question 1e
 '''
 def posted_after(c_lst, dt):
-    pass
+    content_before_dt = []
+    for content in c_lst:
+        if (content.publication_date < dt): content_before_dt.append(content)
+    return content_before_dt    
