0ebab01e95062845a5c6e0adc015db48864583f4
Jayant Sani <jayant.sani@gmail.com>
2014-03-12 15:50:05 -0400

    Complete assignment2

--- assignment2/question1.py
+++ assignment2/question1.py
@@ -19,12 +19,11 @@ class Content(object):
         self.publication_date = datetime.date.today()
 
     def show(self):
-        print "<Title: {0}\nSubtitle: {1}\nCreator: {2}\nPublication Date: {3}>"
-        .format(self.title, self.subtitle, self.creator, self.publication_date.__str__())
+        print "<Title: {0}\nSubtitle: {1}\nCreator: {2}\nPublication Date: {3}>".format(self.title, self.subtitle, self.creator, self.publication_date.__str__())
 
     def matches_url(self, url):
         pattern = r'http://thecrimson.com/(\w+)/(\d{4})/(\d{1,2})/(\d{1,2})/(\w+)'
-        return !(re.search(pattern, url) is None)
+        return not (re.search(pattern, url) is None)
 
     '''
     Question 1e
@@ -38,13 +37,13 @@ class Content(object):
             split_title = c_lst[i].title.split()
             slug = '-'.join(split_title)
 
-            pattern = r'http://thecrimson.com/(\w+)/{0}/{1}/{2}/{3}/'
-            .format(c_list[i].publication_date.year, c_list[i].publication_date.month, c_list[i].publication_date.day, slug)
-            if (!(re.search(pattern, url) is None): matched_content.append(c_lst[i])
+            pattern = r'http://thecrimson.com/(\w+)/{0}/{1}/{2}/{3}/'.format(c_list[i].publication_date.year, c_list[i].publication_date.month, c_list[i].publication_date.day, slug)
+            if (not (re.search(pattern, url) is None)):
+                matched_content.append(c_lst[i])
         
         if (len(matched_content) > 1): 
             print "Error - More than one content object matches the URL!"
-        else if:
+        elif (len(matched_content) == 0):
             print "No content matched"    
         else:
             return matched_content[0]
@@ -62,8 +61,7 @@ class Article(Content):
         self.related_image = image
 
     def show(self):
-        print "<Title: {0}\n Subtitle: {1}\nCreator: {2}\nPublication Date: {3}\nRelated Image: {4}>"
-        .format(self.title, self.subtitle, self.creator, self.publication_date.__str__(), self.related_image)
+        print "<Title: {0}\n Subtitle: {1}\nCreator: {2}\nPublication Date: {3}\nRelated Image: {4}>".format(self.title, self.subtitle, self.creator, self.publication_date.__str__(), self.related_image)
 
 class Picture(Content):
     '''
@@ -78,8 +76,7 @@ class Picture(Content):
         self.image_file = image_file
 
     def show(self):
-        print "<Title: {0}\n Subtitle: {1}\nCreator: {2}\nPublication Date: {3}\nImage File: {4}>"
-        .format(self.title, self.subtitle, self.creator, self.publication_date.__str__(), self.image_file)
+        print "<Title: {0}\n Subtitle: {1}\nCreator: {2}\nPublication Date: {3}\nImage File: {4}>".format(self.title, self.subtitle, self.creator, self.publication_date.__str__(), self.image_file)
 
 '''
 Question 1e
@@ -87,5 +84,6 @@ Question 1e
 def posted_after(c_lst, dt):
     content_before_dt = []
     for content in c_lst:
-        if (content.publication_date < dt): content_before_dt.append(content)
+        if (content.publication_date < dt): 
+            content_before_dt.append(content)
     return content_before_dt    
